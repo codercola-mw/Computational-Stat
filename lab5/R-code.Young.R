aeb5897b43d0f1317d68c99d9d72b700a52a1622
Jooyoung Lee <jy.leellen@gmail.com>
2020-02-21 12:11:19 +0100

    adding code

--- lab5/R-code.Young.R
+++ lab5/R-code.Young.R
@@ -17,6 +17,8 @@ plot(x=data$Day_of_year, y=data$Draft_No,
 loess_fit <- loess(Draft_No~Day_of_year, data=data)
 y_hat <- predict(loess_fit, newdata = data$Day_of_year)
 points(y_hat, col="red")
+legend("topright",legend=c("Original Data", "Fitted Points"), 
+       col=c("black", "red"), pch=c(1,1))
 
   ## the lottery does not seem random when the prediction is
   ## made base on loess function. The Draft No seems to be
@@ -32,9 +34,15 @@ statistic <- function(data, vn){
   y_hat <- predict(loess_fit, newdata = data$Day_of_year)
   x_a <- data$Day_of_year[which.min(y_hat)]
   x_b <- data$Day_of_year[which.max(y_hat)]
-  t_stat <- (y_hat[x_b]-y_hat[x_a]) / (x_b-x_a)
+  t_stat <- (max(y_hat)-min(y_hat)) / (x_b-x_a)
   return(t_stat)
 }
 res <- boot(data=data, statistic = statistic, R=2000)
-res
-res$t0
+hist(res$t, breaks=30, main="Histogram of T-Statistics",
+     xlab="T Statistic")
+cat("The p-value of the distribution of T statistics is: ", "\n")
+mean(res$t > 0)
+  ## When the bootstrap is performed, the resulting histogram
+  ## seems that nearly all t-statistics are placed below 0.
+  ## Furthermore, the p-value is very small.
+  ## It would be plausible to conclude that this data is random.
